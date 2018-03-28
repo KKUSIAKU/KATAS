7f62c4e8c146188b0893feafb092675db4fd1dc1
KKUSIAKU <kusiaku.k@gmail.com>
2018-03-28 08:52:48 +0100

    remove comment in three arrays intersection items

--- katas/commonItemsInLargeArray/index.js
+++ katas/commonItemsInLargeArray/index.js
@@ -23,31 +23,7 @@ function common(a, b, c) {
     bIndex = createIndex(b),
     cIndex = createIndex(c),
     result = 0, searchObject;
-  /*
-    for (let i = 0; i < ln1; i++) {
-      if (aIndex.hasOwnProperty(a[i])) {
-        aIndex[a[i]]++;
-      } else {
-        aIndex[a[i]] = 1;
-      }
-    }
   
-    for (let i = 0; i < ln2; i++) {
-      if (bIndex.hasOwnProperty(b[i])) {
-        bIndex[b[i]]++;
-      } else {
-        bIndex[b[i]] = 1;
-      }
-    }
-  
-    for (let i = 0; i < ln3; i++) {
-      if (cIndex.hasOwnProperty(c[i])) {
-        cIndex[c[i]]++;
-      } else {
-        cIndex[c[i]] = 1;
-      }
-    }
-  */
   searchObject = aIndex.length < bIndex.length ? aIndex : bIndex;
   searchObject = searchObject.length < cIndex.length ? searchObject : cIndex;
 
